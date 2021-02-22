9f6de496828a45d4702ca1525a5876b8928c5d1f
Gabriel Araujo <gabriel@kenzie.com.br>
2021-02-22 12:57:01 -0300

    feature: created a base controller with an sum of numbers

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,15 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename), do: "#{filename}.csv" |> File.read() |> handle_file()
+
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Stream.map(&String.to_integer/1)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid File"}}
+end
---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,23 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "When there is a file with a given name, return a sum of the numbers" do
+      response = Numbers.sum_from_file("numbers")
+
+      expected_response = {:ok, %{result: 52}}
+
+      assert response == expected_response
+    end
+
+    test "When there is no file with a given name, return an error" do
+      response = Numbers.sum_from_file("invalid_file")
+
+      expected_response = {:error, %{message: "Invalid File"}}
+
+      assert response == expected_response
+    end
+  end
+end
